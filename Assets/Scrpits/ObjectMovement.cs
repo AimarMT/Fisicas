d21af8481c5f68f3678010fb62af5ac6edc5efa8
AimarMT <aimar.moreno@opendeusto.es>
2025-03-14 09:43:36 +0100

    Comienzo de físicas

---
+++ Assets/Scrpits/ObjectMovement.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectMovement : MonoBehaviour
+{
+    public int speed=1;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        playerMovement();
+
+
+    }
+    private void playerMovement()
+    {
+       
+        if (Input.GetKey(KeyCode.W))
+        {
+            gameObject.transform.position += new Vector3(speed, 0, 0) * Time.deltaTime;
+        };
+        if (Input.GetKey(KeyCode.A))
+        {
+            gameObject.transform.position += new Vector3(0, 0, speed) * Time.deltaTime;
+        };
+        if (Input.GetKey(KeyCode.S))
+        {
+            gameObject.transform.position += new Vector3(-speed, 0, 0) * Time.deltaTime;
+        };
+        if (Input.GetKey(KeyCode.D))
+        {
+            gameObject.transform.position += new Vector3(0, 0, -speed) * Time.deltaTime;
+        };
+       
+    }
+}
